6bfadb1b91a12d51fab58eb8d33fa24fd9d1491a
shsieh <shsieh@starbucks.com>
2022-10-27 20:42:01 -0700

    session 2 checkin

--- GildedRose.cs
+++ GildedRose.cs
@@ -14,76 +14,111 @@ namespace GildedRose
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (IsDefaultItems(i)) //default products with default rule of decrease quality by 1
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    DefaultItemRules(i);
                 }
-                else
+                else // Aged Brie and Backstage passes
                 {
-                    if (Items[i].Quality < 50)
+                    if (Items[i].Name == "Aged Brie")
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        AgedBrieRules(i);
+                    }
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        BackstagePassesRules(i);
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].SellIn < 0 && Items[i].Name != "Aged Brie")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
+                        if (Items[i].Quality > 0)
+                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                DecreaseQuality(i);
                             }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
                     }
                 }
+                else if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Quality < 50) IncreaseQuality(i);
+                }
             }
+        }
+
+        private void DefaultItemRules(int i)
+        {
+            if (Items[i].Quality > 0)
+                DecreaseQuality(i);
+            DecreaseSellIn(i);
+        }
+
+        private void AgedBrieRules(int i)
+        {
+            if (Items[i].Quality < 50) IncreaseQuality(i);
+            DecreaseSellIn(i);
+        }
+        
+        private bool IsDefaultItems(int i)
+        {
+            return Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Name != "Sulfuras, Hand of Ragnaros";
+        }
+
+        private void BackstagePassesRules(int i)
+        {
+            if (Items[i].Quality < 50) IncreaseQuality(i);
+            switch (Items[i].SellIn < 11)
+            {
+                case true:
+                {
+                    switch (Items[i].Quality < 50)
+                    {
+                        case true:
+                            IncreaseQuality(i);
+                            break;
+                    }
+
+                    break;
+                }
+            }
+
+            switch (Items[i].SellIn < 6)
+            {
+                case true:
+                {
+                    switch (Items[i].Quality < 50)
+                    {
+                        case true:
+                            IncreaseQuality(i);
+                            break;
+                    }
+
+                    break;
+                }
+            }
+            DecreaseSellIn(i);
+        }
+
+        private void IncreaseQuality(int i)
+        {
+            Items[i].Quality = Items[i].Quality + 1;
+        }
+
+        private void DecreaseQuality(int i)
+        {
+            Items[i].Quality = Items[i].Quality - 1;
+        }
+
+        private void DecreaseSellIn(int i)
+        {
+            Items[i].SellIn = Items[i].SellIn - 1;
         }
     }
 }
