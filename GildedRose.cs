3d524a778ba539fb49e06bfff8c575cc18a74acf
shsieh <shsieh@starbucks.com>
2022-10-28 11:15:06 -0700

    refactor

--- GildedRose.cs
+++ GildedRose.cs
@@ -18,17 +18,15 @@ namespace GildedRose
                 {
                     DefaultItemRules(i);
                 }
-                else // Aged Brie and Backstage passes
-                {
-                    if (Items[i].Name == "Aged Brie")
-                    {
-                        AgedBrieRules(i);
-                    }
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        BackstagePassesRules(i);
-                    }
+                if (Items[i].Name == "Aged Brie")
+                {
+                    AgedBrieRules(i);
+                }
+
+                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    BackstagePassesRules(i);
                 }
             }
         }
